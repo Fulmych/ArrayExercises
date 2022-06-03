382c9bb68743d07fcdfb77456a346e4c7f133e4c
Julius <105441072+Fulmych@users.noreply.github.com>
2022-06-03 15:47:59 +0200

    Add files via upload

---
+++ ArrayExercise1/src/main/java/application/ArrayExercise1.java
@@ -0,0 +1,14 @@
+package application;
+import java.util.Arrays;
+public class ArrayExercise1 {
+    public static void main(String[] args) {
+        int array[] = new int [6];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+        array[4] = 5;
+        array[5] = 6;
+        System.out.println(array[0] + "" + array[1] + " " + array[2] + "" + array[3] + " " + array[4] + "" + array[5]);
+    }
+}
