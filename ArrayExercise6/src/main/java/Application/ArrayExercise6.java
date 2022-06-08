4db1bc49ce6dd13f408c1ab99146958676ea044e
Julius <105441072+Fulmych@users.noreply.github.com>
2022-06-08 14:03:10 +0200

    Add files via upload

---
+++ ArrayExercise6/src/main/java/Application/ArrayExercise6.java
@@ -0,0 +1,8 @@
+package Application;
+public class ArrayExercise6 {
+    public static void main(String[] args) {
+        double[] average = {43, 5, 23, 17, 2, 14};
+        double total = (average[0] + average[1] + average[2] + average[3] + average[4] + average[5]) / 6;
+        System.out.println(total);
+    }
+}
