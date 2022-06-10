885e0b3776e45a1abcec82bccb0834b4de84ad18
Julius <105441072+Fulmych@users.noreply.github.com>
2022-06-10 14:01:20 +0200

    Add files via upload

---
+++ ArrayExercise12/src/main/java/Application/ArrayExercise12.java
@@ -0,0 +1,7 @@
+package Application;
+public class ArrayExercise12 {
+    public static void main(String[] args) {
+        int[][] array1 = {{1, 2, 3}, {1, 2, 3}};
+        System.out.println(array1[0][0] + "\t" + (array1[0][1]*array1[1][1]) + "\t" + (array1[0][2]*array1[1][2]));
+    }
+}
